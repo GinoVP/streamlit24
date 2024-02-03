be1dc0fba1f80342c6577b0a9995b958de9d3b4a
GinoVP <158537713+GinoVP@users.noreply.github.com>
2024-02-02 21:19:41 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 import numpy as np
 
+from datetime import time
 
 
 st.title('UPC Data Science 2024')
@@ -15,3 +16,11 @@ n = st.slider("cant. ventas", 5,100, step=1)
 chart_data = pd.DataFrame(np.random.randn(n),columns=['ventas'])
 
 st.line_chart(chart_data)
+
+
+
+
+appointment = st.slider(
+ "Programe la asesoria:",
+ value=(time(11, 30), time(12, 45)))
+st.write("Esta agendado para:", appointment)
