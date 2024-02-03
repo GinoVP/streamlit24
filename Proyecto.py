526f1eca976331e564ba1bf454a1893d5f6e51e6
GinoVP <158537713+GinoVP@users.noreply.github.com>
2024-02-02 21:23:35 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -17,10 +17,8 @@ chart_data = pd.DataFrame(np.random.randn(n),columns=['ventas'])
 
 st.line_chart(chart_data)
 
-# Streamlit: Slider de horario
-appointment = st.slider(
- "Programe la asesoria:",
- value=(time(11, 30), time(12, 45)))
-st.write("Esta agendado para:", appointment)
-
+d = st.date_input(
+ "Fecha de cumpleaños",
+ datetime.date(2019, 7, 6))
+st.write('Tu cumpleños es:', d)
 
