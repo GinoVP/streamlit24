732fe8c3ae9d843cc468045243ec2128f9f3e968
GinoVP <158537713+GinoVP@users.noreply.github.com>
2024-02-02 21:32:17 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -18,9 +18,9 @@ chart_data = pd.DataFrame(np.random.randn(n),columns=['ventas'])
 
 st.line_chart(chart_data)
 
-n = st.slider("n", 5,100, step=1)
-chart_data = pd.DataFrame(np.random.randn(n),columns=['data'])
-st.line_chart(chart_data)
+df = pd.DataFrame(
+ np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+ columns=['lat', 'lon'])
 
 option = st.selectbox(
  '¿Cómo desearía ser contactado/a?',
