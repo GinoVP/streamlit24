7fa86591a6ad67dae4e99134b9cff539eff3b79f
GinoVP <158537713+GinoVP@users.noreply.github.com>
2024-02-02 21:39:26 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -15,9 +15,15 @@ n = st.slider("cant. ventas", 5,100, step=1)
 chart_data = pd.DataFrame(np.random.randn(n),columns=['ventas'])
 st.line_chart(chart_data)
 
-
+#Creando Slider de fecha
 start_time = st.slider(
  "Ver casos ocurridos en",
  value=datetime(2020, 1, 1, 9, 30),
  format="DD/MM/YY - hh:mm")
 st.write("Fecha seleccionada:", start_time)
+
+#Creando Calendario
+d = st.date_input(
+ "Fecha de cumpleaños",
+ datetime.date(2019, 7, 6))
+st.write('Tu cumpleños es:', d)
