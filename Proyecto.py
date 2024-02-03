bc5b85cc67c7c623851f4cbbbc50aef4c4707a59
GinoVP <158537713+GinoVP@users.noreply.github.com>
2024-02-02 21:38:18 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 import numpy as np
 
+import datetime
+
 #Ingresando Titulo
 st.title('UPC Data Science 2024')
 
@@ -12,3 +14,10 @@ st.header('Simulador Ventas')
 n = st.slider("cant. ventas", 5,100, step=1)
 chart_data = pd.DataFrame(np.random.randn(n),columns=['ventas'])
 st.line_chart(chart_data)
+
+
+start_time = st.slider(
+ "Ver casos ocurridos en",
+ value=datetime(2020, 1, 1, 9, 30),
+ format="DD/MM/YY - hh:mm")
+st.write("Fecha seleccionada:", start_time)
