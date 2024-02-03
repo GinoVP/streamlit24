d61503dc296cd8b6644a80e85575bed6d1db1895
GinoVP <158537713+GinoVP@users.noreply.github.com>
2024-02-02 21:42:07 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -29,3 +29,9 @@ d = st.date_input(
  "Fecha de cumpleaños",
  dt.date(2019, 7, 6))
 st.write('Tu cumpleños es:', d)
+
+#Creando lista de seleccion
+option = st.selectbox(
+ '¿Cómo desearía ser contactado/a?',
+ ('Email', 'Teléfono', 'Whatsapp'))
+st.write('Seleccionó:', option
